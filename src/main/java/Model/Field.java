0135b6598fa4541218f558f678f9e0ffa480c5a1
SamaPozitiff <sama.pozitiff@gmail.com>
2022-10-15 22:32:41 +0300

    add remove value from row + tests

--- src/main/java/Model/Field.java
+++ src/main/java/Model/Field.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Field {
-    private int [][] field;
-    private int [] rowScores;
+    private int[][] field;
+    private int[] rowScores;
     private int finalScore;
     private static final int FIELD_SIZE = 3;
 
-    public Field(){
+    public Field() {
         field = new int[3][3];
         rowScores = new int[3];
     }
@@ -26,9 +26,9 @@ public class Field {
         return finalScore;
     }
 
-    public void addToField(int row, int value){
-        for (int i = 0; i < FIELD_SIZE; i++){
-            if(field[row][i] == 0){
+    public void addValueToRow(int row, int value) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            if (field[row][i] == 0) {
                 field[row][i] = value;
                 break;
             }
@@ -37,32 +37,59 @@ public class Field {
         countFinalScore();
     }
 
-    public void countRowScore (int row){
+    public void countRowScore(int row) {
         rowScores[row] = 0;
         Map<Integer, Integer> values = new HashMap<>();
-        for (int i = 0; i < FIELD_SIZE; i++){
-            if(values.containsKey(field[row][i])){
-                values.put(field[row][i], values.get(field[row][i])+1);
+        for (int i = 0; i < FIELD_SIZE; i++) {
+            if (values.containsKey(field[row][i])) {
+                values.put(field[row][i], values.get(field[row][i]) + 1);
             } else {
                 values.put(field[row][i], 1);
             }
         }
-        for(Map.Entry<Integer, Integer> entry: values.entrySet()){
-            if (entry.getValue() == 1){
-                rowScores[row]+= entry.getKey();
-            } else if(entry.getValue() == 2){
-                rowScores[row]+= (entry.getKey() * 2)*2;
-            }else if (entry.getValue() == 3){
-                rowScores[row]+= ((entry.getKey() * 2)*2)*2 + entry.getKey();
+        for (Map.Entry<Integer, Integer> entry : values.entrySet()) {
+            if (entry.getValue() == 1) {
+                rowScores[row] += entry.getKey();
+            } else if (entry.getValue() == 2) {
+                rowScores[row] += (entry.getKey() * 2) * 2;
+            } else if (entry.getValue() == 3) {
+                rowScores[row] += ((entry.getKey() * 2) * 2) * 2 + entry.getKey();
             }
         }
     }
 
-    public void countFinalScore(){
+    public void countFinalScore() {
         finalScore = 0;
-        for (int i = 0; i < rowScores.length; i++){
-            finalScore+= rowScores[i];
+        for (int i = 0; i < rowScores.length; i++) {
+            finalScore += rowScores[i];
         }
     }
+
+    public void removeValueFromRow(int row, int value) {
+        if (field[row] == null) {
+            return;
+        }
+        for (int i = 0; i < field[row].length; i++) {
+            if (field[row][i] == value) {
+                field[row][i] = 0;
+            }
+        }
+        int j = field[row].length - 1;
+        while (j > 0) {
+            if (field[row][j] == 0) {
+                j--;
+            } else if (field[row][j - 1] == 0) {
+                field[row][j - 1] = field[row][j];
+                field[row][j] = 0;
+                j = field[row].length - 1;
+            } else {
+                j--;
+            }
+        }
+        countRowScore(row);
+        countFinalScore();
+    }
+
+
 
 }
--- src/test/java/TestField.java
+++ src/test/java/TestField.java
@@ -6,14 +6,35 @@ public class TestField {
     @Test
     public void test1(){
         Field field = new Field();
-        field.addToField(0, 5);
-        field.addToField(0, 5);
-        field.addToField(2, 3);
-        field.addToField(2, 3);
-        field.addToField(2, 3);
-        field.addToField(1, 4);
+        field.addValueToRow(0, 5);
+        field.addValueToRow(0, 5);
+        field.addValueToRow(2, 3);
+        field.addValueToRow(2, 3);
+        field.addValueToRow(2, 3);
+        field.addValueToRow(1, 4);
         Assert.assertEquals(new int[][]{{5,5,0},{4,0,0},{3,3,3}}, field.getField());
         Assert.assertEquals(new int[]{20,4,27}, field.getRowScores());
         Assert.assertEquals(51, field.getFinalScore());
+    }
+    @Test
+    public void test2(){
+        Field field = new Field();
+        field.addValueToRow(0, 5);
+        field.addValueToRow(0, 4);
+        field.addValueToRow(0, 3);
+        Assert.assertEquals(new int[][]{{5,4,3},{0,0,0},{0,0,0}}, field.getField());
+        Assert.assertEquals(new int[]{12,0,0}, field.getRowScores());
+        field.removeValueFromRow(0, 5);
+        Assert.assertEquals(new int[][]{{4,3,0},{0,0,0},{0,0,0}}, field.getField());
+    }
+
+    @Test
+    public void test3(){
+        Field field = new Field();
+        field.addValueToRow(1, 6);
+        field.addValueToRow(1,6);
+        field.addValueToRow(1,4);
+        field.removeValueFromRow(1,6);
+        Assert.assertEquals(new int[][] {{0,0,0},{4,0,0},{0,0,0}}, field.getField());
     }
 }
